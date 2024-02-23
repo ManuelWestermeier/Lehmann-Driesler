2aedb828f5b4b387a8d6cbba35ee1ee6c573eb33
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-02-23 15:37:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const observer = new IntersectionObserver(entrees => {
     entrees.forEach(entry => {
-        if (!entry.isIntersecting)
+        if (!entry.isIntersecting) {
             entry.target.classList.add("hidden-elem")
-        else
+        }
+        else {
             entry.target.classList.remove("hidden-elem")
+        }
+
+        if (entry.target.classList.contains("user")) {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("user-animated")
+            }
+            else {
+                entry.target.classList.remove("user-animated")
+            }
+        }
     })
 })
 
